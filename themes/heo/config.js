0305bb48500ff3420a8658e4674317c3ef2bd1c6
kforris <58551353+kforris@users.noreply.github.com>
2023-12-25 08:27:21 -0500

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,26 +9,25 @@ const CONFIG = {
     { title: '不知道躺了做什么？', url: 'https://docs.tangly1024.com' }
   ],
 
-  // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '享受',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
-
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '好吃不胖饭饭', url: '/tag/饭饭' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
-  HEO_HERO_CATEGORY_4: { title: '默默默默', url: '/tag/阿斯顿发' },
 
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
-
+  
+// 英雄区(首页顶部大卡)
+  HEO_HERO_TITLE_1: '体验',
+  HEO_HERO_TITLE_2: '与躺平感悟',
+  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '这是xwzx展示地',
