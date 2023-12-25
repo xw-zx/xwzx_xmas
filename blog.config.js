dc6e46476f3d9417311171c9d748188106d5e7fa
kforris <58551353+kforris@users.noreply.github.com>
2023-12-25 10:09:41 -0500

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -195,8 +195,8 @@ const BLOG = {
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
   WIDGET_PET_LINK:
         process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
-        'https://github.com/imuncle/live2d/blob/master/live2d_3/model/Azue%20Lane(JP)/aidang_2/aidang_2.model3.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
-  WIDGET_PET_SWITCH_THEME: process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || true, // 点击宠物挂件切换博客主题
+        'https://raw.githubusercontent.com/imuncle/live2d/master/live2d_3/model/Azue%20Lane(JP)/aidang_2/aidang_2.model3.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
+  WIDGET_PET_SWITCH_THEME: process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || false, // 点击宠物挂件切换博客主题
 
   // 音乐播放插件
   MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || false, // 是否使用音乐播放插件
