544599f9633d36a29c082d79ea983e4ef3aaed79
kforris <58551353+kforris@users.noreply.github.com>
2023-12-25 10:57:52 -0500

    Update ThemeSwitch.js

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -56,16 +56,16 @@ const ThemeSwitch = () => {
                 </div>
 
                 {/* 主题切换按钮 */}
-                <div className="text-sm flex items-center group-hover:w-32 transition-all duration-200">
-                    <i className="fa-solid fa-palette w-5" />
-                    <div className='w-0 group-hover:w-24 transition-all duration-200 overflow-hidden'>
-                        <select value={currentTheme} onChange={onThemeSelectChange} name="themes" className='pl-1 bg-gray-50 dark:bg-black appearance-none outline-none dark:text-white uppercase cursor-pointer'>
-                            {THEMES?.map(t => {
-                              return <option key={t} value={t}>{t}</option>
-                            })}
-                        </select>
-                    </div>
-                </div>
+                // <div className="text-sm flex items-center group-hover:w-32 transition-all duration-200">
+                //     <i className="fa-solid fa-palette w-5" />
+                //     <div className='w-0 group-hover:w-24 transition-all duration-200 overflow-hidden'>
+                //         <select value={currentTheme} onChange={onThemeSelectChange} name="themes" className='pl-1 bg-gray-50 dark:bg-black appearance-none outline-none dark:text-white uppercase cursor-pointer'>
+                //             {THEMES?.map(t => {
+                //               return <option key={t} value={t}>{t}</option>
+                //             })}
+                //         </select>
+                //     </div>
+                // </div>
             </div>
         </Draggable>
 
